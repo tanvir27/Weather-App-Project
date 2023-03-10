b0ea8c1c4c12b6abd658220dd05bc82f89e414da
Tanvir Hasan <87339504+tanvir27@users.noreply.github.com>
2023-03-10 23:48:50 +0600

    updated js file

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
-let button = document.getElementById('button');
-let inputValue = document.getElementById('inputValue');
-let cityName = document.getElementById('cityName');
-let temparature = document.getElementById('temp');
-let description = document.getElementById('desc');
-let newImage = document.getElementById('image');
+const api_key = `5ce92a0475ee3fe1416783adf522e9be`;
 
-button.addEventListener('click', function(){
-    fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=5ce92a0475ee3fe1416783adf522e9be')
+// fetch API
+const loadTemperatures = city => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`;
+    fetch(url)
     .then(response => response.json())
-    .then(data =>{
-        console.log(data);
-        // get the api data from weatherapimain website
-        let updateName = data['name'];
-        let updateTemp = data['main'] ['temp'];
-        let updateDesc = data['weather'] [0] ['description'];
-        // let updateImage = data ['image'];
+    .then(data => displayTemperature(data))
+}
 
-        // updating new weather data
-        // newImage.innerText = updateImage;
-        cityName.innerText = updateName; 
-        temparature.innerText = updateTemp;
-        description.innerText = updateDesc;
-    })
-    // if wrong input then it work
-    .catch(error => alert("Wrong City Name!"))
+// display temperature and weather in city
+const displayTemperature = data => {
+    // console.log(data);
+   setInnerTextById('temp', data.main.temp);
+   setInnerTextById('desc', data.weather[0].main);
+}
+
+// set inner Text
+
+const setInnerTextById = (id, set_text) =>{
+    const temperature = document.getElementById(id);
+    temperature.innerText = set_text;
+}
+
+// search button handler click
+document.getElementById('src_btn').addEventListener('click',function(){
+    const searchField = document.getElementById('search_field');
+    const cityName = searchField.value;
+    searchField.value = '';
+    document.getElementById('city_name').innerText = cityName;
+    loadTemperatures(cityName)
 })
+
+//enter hit 
+document.getElementById('search_field').addEventListener("keypress", function(e){
+    if (e.key === "Enter") {
+        e.preventDefault();
+        document.getElementById("src_btn").click();
+      }
+})
+
+loadTemperatures('dhaka');
